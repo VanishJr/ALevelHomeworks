8149a970dcaaf2dd2d03e533351b83f9d4785089
Vanish <91227368+VanishJr@users.noreply.github.com>
2021-11-10 09:48:18 -0800

    add files Lesson01

---
+++ Lesson01/Lesson01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ivan";
+            string lastName = "Logutov";
+            Console.WriteLine(lastName + " " + name);
+            int age = 17;
+            int futureLife = 40;
+            int newLife = futureLife - age;
+            Console.WriteLine("Time to start a new life: " + newLife);
+        }
+    }
+}
